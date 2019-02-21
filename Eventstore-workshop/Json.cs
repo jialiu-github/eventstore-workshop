1ad5494d394c28f0b978ca9dbf3cc80bf26d9f7b
zhuzhenguang <zgzhu@thoughtworks.com>
2019-02-21 13:21:32 +0800

    add Json

---
+++ Eventstore-workshop/Json.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+
+namespace Eventstore_workshop
+{
+    public class Json
+    {
+        public static byte[] SerializeToBytes(object o)
+        {
+            return Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(o));
+        }
+
+        public static TE DeserializeFromBytes<TE>(byte[] bytes)
+        {
+            return JsonConvert.DeserializeObject<TE>(Encoding.UTF8.GetString(bytes));
+        }
+    }
+}
